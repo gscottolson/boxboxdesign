870bbde98c349e7f6ff66c1d3a3346bb1ed9c56d
gscottolson <scott@gscottolson.com>
2024-01-07 13:10:38 -0800

    Fix close button SVG

--- app/iracing/2024s1/@modal/(..)series/[id]/modal.tsx
+++ app/iracing/2024s1/@modal/(..)series/[id]/modal.tsx
@@ -34,7 +34,7 @@ export function Modal({ children }: { children: React.ReactNode }) {
       >
         {children}
         <button onClick={onDismiss} className="absolute top-0 right-0 size-[64px] p-[16px] text-center">
-          <svg className="inline-block" width="24" height="24" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg className="inline-block" width="20" height="20" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M52.7598 4.69434L4.75977 52.6943M4.75977 4.69434L52.7598 52.6943" stroke="#32545B" strokeWidth="8" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>
         </button>
