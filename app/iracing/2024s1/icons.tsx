a06dc527443dc9390e384a8f57df2a13c5fef068
Scott Olson <s.olson@microsoft.com>
2024-01-07 19:15:18 -0800

    Icons & logos

---
+++ app/iracing/2024s1/icons.tsx
@@ -0,0 +1,66 @@
+export function Close() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M18 2L10 10L18 18"
+        stroke="#2B3C59"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M2 2L10 10L2 18"
+        stroke="#2B3C59"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function Back() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14 2L6 10L14 18"
+        stroke="#2B3C59"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function Download() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2 5.5V2H10H18V5.5M10 18L3.5 11.5M10 18L16.5 11.5M10 18V5.5"
+        stroke="#F5F5F5"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
