4c46f91cdba191f5e3259fc90c43432285a3f522
Scott Olson <s.olson@microsoft.com>
2024-01-08 19:58:47 -0800

    Improve padding

--- app/iracing/2024s1/layout.tsx
+++ app/iracing/2024s1/layout.tsx
@@ -12,7 +12,7 @@ export default function Layout(props: {
   modal: React.ReactNode
  }) {
   return (
-    <div className="lg:p-12 relative">
+    <div className="lg:px-24 lg:py-16 relative">
       <div className="m-auto max-w-7xl flex-grow border-t-8 border-[#A7C3DD] bg-white200 drop-shadow-2xl ">
         <header className="text-center tracking-tighter text-gray700">
           <h1 className="pt-12 text-4xl  font-light antialiased">Official Series Schedule Posters</h1>
