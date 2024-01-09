8240772e3bb53b1a20c9a99753e2977d27825f92
Scott Olson <s.olson@microsoft.com>
2024-01-09 08:46:07 -0800

    Adjust prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -2,5 +2,7 @@ const styleguide = require('@vercel/style-guide/prettier');
 
 module.exports = {
   ...styleguide,
+  printWidth: 120,
+  tabWidth: 4,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
 };
