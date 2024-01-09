48296154b2992f0af857b473b2749aa4302354c0
gscottolson <scott@gscottolson.com>
2024-01-09 14:54:25 -0800

    Visual tweaks

--- app/iracing/2024s1/series-card.tsx
+++ app/iracing/2024s1/series-card.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { OfficialSeries, getSeriesURL } from '../schedule-list';
+import { License, OfficialSeries, getSeriesURL } from '../schedule-list';
 import Balancer from 'react-wrap-balancer';
 import Link from 'next/link';
 
@@ -8,43 +8,47 @@ interface SeriesCardProps {
   priority: boolean;
 }
 
+function getLicenseBorder(licenseClass: License) {
+  if (licenseClass === 'Rookie') return 'border-rookie';
+  if (licenseClass === 'D') return 'border-classD';
+  if (licenseClass === 'C') return 'border-classC';
+  if (licenseClass === 'B') return 'border-classB';
+  if (licenseClass === 'A') return 'border-classA';
+  return 'border-transparent';
+}
+
 export function SeriesCard({ series, priority }: SeriesCardProps) {
-  const borderColor = {
-    Rookie: 'border-[#92342E]',
-    D: 'border-[#F98406]',
-    C: 'border-[#D3A400]',
-    B: 'border-[#3C6D56]',
-    A: 'border-[#315187]',
-    none: 'border-transparent',
-  };
-
-  const seriesBorder = borderColor[series.licenseClass || 'none'];
   const size = 240;
-
+  const borderColor = getLicenseBorder(series.licenseClass);
   return (
-    <Link href={getSeriesURL(series.seriesId)} passHref scroll={false}>
+    <Link
+      href={getSeriesURL(series.seriesId)}
+      passHref
+      scroll={false}
+      className="group"
+    >
       <div className="w-card text-teal800">
-        {!series.src ? (
-          <div className="h-card flex items-center justify-center bg-white300 p-4 align-middle leading-loose text-gray700">
-            Coming soon
-          </div>
-        ) : (
-          <Image
-            alt={`styled image of a schedule poster for ${series.name}`}
-            src={series.src}
-            width={size}
-            height={size}
-            priority={priority}
-          />
-        )}
-
         <div
-          className={`border-t-4 ${seriesBorder} flex min-h-4 flex-col px-6 `}
+          className={`border-b-2 ${borderColor} group-active:scale-card flex min-h-4 scale-100 flex-col shadow-xl`}
         >
-          <h2 className="basis-full pt-2 text-center leading-5">
-            <Balancer>{series.name}</Balancer>
-          </h2>
+          {!series.src ? (
+            <div className="h-card bg-white400 flex items-center justify-center p-4 align-middle leading-loose text-gray700">
+              Coming soon
+            </div>
+          ) : (
+            <Image
+              alt={`styled image of a schedule poster for ${series.name}`}
+              src={series.src}
+              width={size}
+              height={size}
+              priority={priority}
+            />
+          )}
         </div>
+
+        <h2 className="basis-full pt-2 text-center leading-5">
+          <Balancer>{series.name}</Balancer>
+        </h2>
       </div>
     </Link>
   );
