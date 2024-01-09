58eff5e5d662d749358e53573a94108c6ff8f194
Scott Olson <s.olson@microsoft.com>
2024-01-09 11:11:43 -0800

    Reformat

--- app/iracing/2024s1/layout.tsx
+++ app/iracing/2024s1/layout.tsx
@@ -1,21 +1,23 @@
-import { Metadata } from "next";
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: {
     template: '%s | iRacing 2024 S1 Schedule Posters',
     default: 'iRacing 2024 S1 Schedule Posters', // a default is required when creating a template
   },
-}
+};
 
 export default function Layout(props: {
-  children: React.ReactNode
-  modal: React.ReactNode
- }) {
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}) {
   return (
-    <div className="lg:px-24 lg:py-16 relative">
-      <div className="m-auto max-w-7xl flex-grow border-t-8 border-[#A7C3DD] bg-white200 drop-shadow-2xl ">
+    <div className="relative lg:px-24 lg:py-16">
+      <div className="m-auto max-w-7xl flex-grow drop-shadow-2xl">
         <header className="text-center tracking-tighter text-gray700">
-          <h1 className="pt-12 text-4xl  font-light antialiased">Official Series Schedule Posters</h1>
+          <h1 className="pt-12 text-4xl font-light antialiased">
+            Official Series Schedule Posters
+          </h1>
           <h2 className="p-4 pt-0 text-xl font-medium tracking-wide">
             iRacing 2024 Season 1
           </h2>
@@ -28,5 +30,3 @@ export default function Layout(props: {
     </div>
   );
 }
-
-
