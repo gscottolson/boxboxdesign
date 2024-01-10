c6d6ebd64a1fc6a87b149b1ec5506f3f4d39f0a7
Scott Olson <s.olson@microsoft.com>
2024-01-09 16:41:52 -0800

    BG image

--- app/iracing/layout.tsx
+++ app/iracing/layout.tsx
@@ -1,7 +1,8 @@
 export default function Layout(props: { children: React.ReactNode }) {
   const fromTo = 'from-white200 to-white400'; // maintaining order, which Prettier tried to destroy
+  //bg-gradient-to-b ${fromTo} bg-fixed
   return (
-    <div className={`min-h-dvh bg-gradient-to-b ${fromTo} bg-fixed`}>
+    <div className={`bg-siteGradient min-h-dvh bg-[#BDCDD2] bg-repeat-x`}>
       {props.children}
     </div>
   );
