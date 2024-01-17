a6a13cda2c4d9d95ba0c1b772a82b6f201ab1501
Scott Olson <s.olson@microsoft.com>
2024-01-17 12:25:38 -0800

    Add metadata to details page

--- app/site.ts
+++ app/site.ts
@@ -1,9 +1,13 @@
 const STATIC_TITLE = 'iRacing Official Series Schedules';
 
 export function getSiteTitle(item?: string) {
-  return item ? `${item} | ${STATIC_TITLE}` : STATIC_TITLE;
+    return item ? `${item} | ${STATIC_TITLE}` : STATIC_TITLE;
 }
 
 export function getSiteTitleTemplate() {
-  return `%s | ${STATIC_TITLE}`;
+    return `%s | ${STATIC_TITLE}`;
+}
+
+export function getDetailTitle(token: string) {
+    return `${token} | ${STATIC_TITLE}`;
 }
