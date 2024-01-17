09f2a2e9bde6a5a8a5de10427e47c05676af643f
Scott Olson <s.olson@microsoft.com>
2024-01-17 11:14:26 -0800

    Update home colors

--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,18 @@ import { getSiteTitle as getiRacingSiteTitle } from './site';
 import { getDisciplineURL } from './iracing/data/series-util';
 
 export default function Page() {
-    const background = 'url(/home/speckled.png), linear-gradient(180deg, #C7BEA610 0%, #A3988880 100%), #E5E3E1';
-    const backgroundBlendMode = 'multiply, normal, normal';
-    const backgroundSize = '50%';
+    const background = 'linear-gradient(180deg, #EEF5DA 0%, #DFEDD5 100%), #DFEDD5';
 
     return (
         <main
-            className="flex min-h-dvh flex-col items-center justify-center gap-16 p-6 text-[#211F1F]"
-            style={{ background, backgroundBlendMode, backgroundSize }}
+            className="flex min-h-dvh flex-col items-center justify-center gap-16 p-6 text-[#304F70]"
+            style={{ background }}
         >
             <Image width={420} height={420} alt="BoxBoxDesign logo" src="/home/boxbox-circle.svg" />
 
-            <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center text-xl leading-[24px]">
                 <p className="pb-2 font-light">Current projects:</p>
-                <ul className="text-center font-medium leading-[18px]">
+                <ul className="text-center font-medium ">
                     <li>
                         <Link href={getDisciplineURL('Road')} className="hover:underline">
                             {getiRacingSiteTitle()}
