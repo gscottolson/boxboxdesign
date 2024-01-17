b9867e840fbedbdfff58d68d6332326ca96d029f
gscottolson <scott@gscottolson.com>
2024-01-17 09:41:10 -0800

    Improve nav styling

--- app/iracing/2024s1/nav.tsx
+++ app/iracing/2024s1/nav.tsx
@@ -11,26 +11,26 @@ export function Nav({ active }: { active: Discipline }) {
     const isUnranked = active === 'Unranked';
 
     return (
-        <ul className="m-auto flex w-72 flex-wrap justify-center gap-x-8 gap-y-2 px-4 pb-12 pt-4 align-middle font-medium uppercase text-gray700 md:w-full">
-            <li key="a" style={{ textDecoration: isRoad ? 'underline' : 'none' }}>
-                <Link href={getDisciplineURL('Road')}>Road</Link>
-            </li>
-
-            <li key="b" style={{ textDecoration: isOval ? 'underline' : 'none' }}>
-                <Link href={getDisciplineURL('Oval')}>Oval</Link>
-            </li>
-
-            <li key="c" style={{ textDecoration: isDirtOval ? 'underline' : 'none' }}>
-                <Link href={getDisciplineURL('Dirt Oval')}>Dirt Oval</Link>
-            </li>
-
-            <li key="d" style={{ textDecoration: isDirtRoad ? 'underline' : 'none' }}>
-                <Link href={getDisciplineURL('Dirt Road')}>Dirt Road</Link>
-            </li>
-
-            <li key="e" style={{ textDecoration: isUnranked ? 'underline' : 'none' }}>
-                <Link href={getDisciplineURL('Unranked')}>Unranked</Link>
-            </li>
+        <ul className="m-auto flex flex-wrap items-start justify-center gap-2 px-4 pb-12 pt-4 align-middle font-medium uppercase tracking-widest text-gray700 md:w-full md:items-start">
+            <Item key="road" discipline="Road" active={isRoad} />
+            <Item key="oval" discipline="Oval" active={isOval} />
+            <Item key="dirtoval" discipline="Dirt Oval" active={isDirtOval} />
+            <Item key="dirtroad" discipline="Dirt Road" active={isDirtRoad} />
+            <Item key="unranked" discipline="Unranked" active={isUnranked} />
         </ul>
+    );
+}
+
+function Item({ discipline, active }: { discipline: Discipline; active: boolean }) {
+    const classes = active ? 'text-white100 bg-teal800/50 inner-shadow' : 'hover:bg-teal800/10';
+    return (
+        <li key="e" className={`${classes} rounded-md px-4 py-1 transition-all`}>
+            <Link
+                className={active ? 'cursor-default drop-shadow-sm' : 'cursor-pointer'}
+                href={active ? '' : getDisciplineURL(discipline)}
+            >
+                {discipline}
+            </Link>
+        </li>
     );
 }
