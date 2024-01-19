824a6d56f6912be4dad2b23fbc7b3c3f79cf5896
Scott Olson <s.olson@microsoft.com>
2024-01-18 16:28:58 -0800

    Add metadataBase

--- app/iracing/page.tsx
+++ app/iracing/page.tsx
@@ -8,15 +8,20 @@ import { getDisciplineURL } from './data/series-util';
 export default function Page() {
     return (
         <main className="flex min-h-dvh flex-col items-center justify-center bg-[#BDCDD2] bg-siteGradient bg-repeat-x text-base font-light leading-[28px] text-teal800 lg:text-xl">
-            <div className="mx-12 mt-12 max-w-[618px] lg:mt-24 lg:max-w-7xl">
+            <div className="mx-4 mt-12 max-w-[618px] lg:mx-12 lg:mt-24 lg:max-w-7xl">
                 <div className="mb-12 flex flex-col gap-4 lg:mb-24 lg:flex-row lg:gap-[44px]">
-                    <div className="h-64 overflow-hidden rounded-sm shadow-2xl lg:h-fit lg:basis-1/2">
-                        <Image
-                            src="/iracing/poster-grid.png"
-                            width={1236 / 2}
-                            height={1008 / 2}
-                            alt="An image depicting a grid of designed racing posters with one poster being highlighted and lifted out of the grid."
-                        />
+                    <div className="flex shrink-0 flex-col justify-between gap-8 lg:basis-1/2 lg:items-end">
+                        <div className="pt-4">
+                            <BoxBoxIconOneColor />
+                        </div>
+                        <div className="inline-block h-auto w-auto rounded-sm shadow-2xl">
+                            <Image
+                                src="/iracing/poster-grid.png"
+                                width={1236 / 2}
+                                height={1008 / 2}
+                                alt="An image depicting a grid of designed racing posters with one poster being highlighted and lifted out of the grid."
+                            />
+                        </div>
                     </div>
 
                     <div className="m-auto flex flex-col lg:basis-1/2 ">
@@ -27,25 +32,29 @@ export default function Page() {
                         </h1>
                         <div>
                             <p className="pb-[28px] pt-[6px]">
-                                I’m Scott. I’m passionate about sim racing and I love using information design to make
-                                things more human.
+                                I’m Scott. <br />
+                                I’m passionate about sim racing. <br />I love designing for humans.
                             </p>
                             <p className="pb-[28px]">
-                                Every three months, the online racing service iRacing releases schedules for the
-                                official series. These schedules arrive as a bundled collection of Word docs smashed
-                                into a PDF. While they are functional, they are not pretty and the design is…lacking.
+                                Every three months, the online racing service{' '}
+                                <a href="https://www.iracing.com" target="__blank">
+                                    iRacing
+                                </a>{' '}
+                                releases schedules for the official series. These schedules are distributed as a bundled
+                                collection of Word docs smashed into a PDF. While they are functional, they are not
+                                pretty and the design is…lacking.
                             </p>
                             <p>
-                                I reworked the layout for each series with the goal of making beautiful and easy-to-read
-                                schedule posters. Most schedules include twelve official weeks, with the track or car
-                                (or both) changing every seven days. Important details about the cars, session date,
-                                session time, weather, and splits are included.
+                                The layout for each series has been updated, with the goal of making beautiful and
+                                easy-to-read schedule posters. Most schedules include twelve official weeks, with the
+                                track or car (or both) changing every seven days. Important details about the cars,
+                                session date, session time, weather, and splits are included.
                             </p>
                         </div>
                     </div>
                 </div>
 
-                <div className="flex flex-col gap-[43px] pb-16 lg:flex-row">
+                <div className="flex flex-col gap-8">
                     <Feature
                         title="The starting grid"
                         src="/iracing/grid-blue.png"
@@ -74,7 +83,7 @@ export default function Page() {
                     </Feature>
                 </div>
 
-                <div className="py-24 text-center text-xl font-normal">
+                <div className="pb-12 pt-24 text-center text-xl font-normal">
                     <BackLink href={getDisciplineURL('Road')} />
                 </div>
             </div>
@@ -86,7 +95,7 @@ export default function Page() {
                     </div>
 
                     <p className="mb-2">
-                        I'm an iRacing enthusiast, motorsports fan and lover of well-designed information.
+                        I’m an iRacing enthusiast, motorsports fan and lover of well-designed information.
                     </p>
 
                     <p className="mb-8">
@@ -109,12 +118,19 @@ export default function Page() {
 
 function Feature(props: { title: string; src: string; altText: string; children: React.ReactNode }) {
     return (
-        <div className="flex flex-col gap-2 lg:basis-1/3 lg:gap-[42px]">
-            <div className="relative h-48 w-full min-w-72 items-center overflow-hidden rounded-sm shadow-2xl lg:h-96">
-                <Image className="object-cover object-top" fill src={props.src} alt={props.altText} />
+        <div className="flex flex-col gap-8 md:flex-row lg:gap-[44px]">
+            <div className="flex shrink-0 md:basis-1/3 lg:basis-1/2 lg:justify-end">
+                <div className="relative h-36 w-full overflow-hidden rounded-sm shadow-2xl md:h-48 md:w-full lg:h-72 lg:w-4/6">
+                    <Image
+                        className="object-cover object-center md:object-top"
+                        fill
+                        src={props.src}
+                        alt={props.altText}
+                    />
+                </div>
             </div>
             <div className="flex w-full flex-col">
-                <h2 className="pb-2 pt-4 text-2xl font-light leading-8 lg:pb-[20px] lg:pt-[8px] lg:text-3xl">
+                <h2 className="pb-2 text-2xl font-light leading-8 lg:pb-[20px] lg:pt-[8px] lg:text-3xl">
                     {props.title}
                 </h2>
                 <p>{props.children}</p>
