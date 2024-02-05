577a3c288bc4f15f00fb64408ab1669c330e799e
Scott Olson <s.olson@microsoft.com>
2024-02-05 15:00:51 -0800

    Move images to project folder

--- app/(ioverlay)/ioverlay/page.tsx
+++ app/(ioverlay)/ioverlay/page.tsx
@@ -18,13 +18,13 @@ export default function Page() {
             </p>
 
             <Image
-                src="/ioverlay/ioverlay-dark.png"
+                src="/project/ioverlay/project/ioverlay-dark.png"
                 width={1280}
                 height={700}
                 alt="iOverlay identity on a dark background"
             />
             <Image
-                src="/ioverlay/ioverlay-light.png"
+                src="/project/ioverlay/project/ioverlay-light.png"
                 width={1280}
                 height={700}
                 alt="iOverlay identity on a light background"
@@ -33,33 +33,33 @@ export default function Page() {
             <h2 className="mb-4 mt-8 text-center text-xl">3d explorations</h2>
 
             <div className="grid grid-flow-row grid-cols-2 gap-[12px]">
-                <Image src="/ioverlay/closeup.jpg" width={634} height={634} alt="" />
-                <Image src="/ioverlay/portrait.jpg" width={634} height={634} alt="" />
-                <Image className="col-span-2" src="/ioverlay/wordmark.jpg" width={1280} height={634} alt="" />
-                <Image src="/ioverlay/clay001.jpg" width={634} height={634} alt="" />
-                <Image src="/ioverlay/clay002.jpg" width={634} height={634} alt="" />
-                <Image src="/ioverlay/isometric.jpg" width={634} height={634} alt="" />
-                <Image src="/ioverlay/steps.jpg" width={634} height={634} alt="" />
+                <Image src="/project/ioverlay/closeup.jpg" width={634} height={634} alt="" />
+                <Image src="/project/ioverlay/portrait.jpg" width={634} height={634} alt="" />
+                <Image className="col-span-2" src="/project/ioverlay/wordmark.jpg" width={1280} height={634} alt="" />
+                <Image src="/project/ioverlay/clay001.jpg" width={634} height={634} alt="" />
+                <Image src="/project/ioverlay/clay002.jpg" width={634} height={634} alt="" />
+                <Image src="/project/ioverlay/isometric.jpg" width={634} height={634} alt="" />
+                <Image src="/project/ioverlay/steps.jpg" width={634} height={634} alt="" />
             </div>
 
             <h2 className="mb-4 mt-8 text-center text-xl">Concept iteration, round 1</h2>
             <div className="m-auto max-w-[1050px]">
-                <Image src="/ioverlay/round001.png" width={1050} height={524} alt="" />
+                <Image src="/project/ioverlay/round001.png" width={1050} height={524} alt="" />
             </div>
 
             <h2 className="mb-4 mt-8 text-center text-xl">Concept iteration, round 2</h2>
             <div className="m-auto max-w-[1050px]">
-                <Image src="/ioverlay/round002.png" width={1050} height={1284} alt="" />
+                <Image src="/project/ioverlay/round002.png" width={1050} height={1284} alt="" />
             </div>
 
             <h2 className="mb-4 mt-8 text-center text-xl">Concept iteration, round 3</h2>
             <div className="m-auto max-w-[1050px]">
-                <Image src="/ioverlay/round003.png" width={1050} height={1020} alt="" />
+                <Image src="/project/ioverlay/round003.png" width={1050} height={1020} alt="" />
             </div>
 
             <h2 className="mb-4 mt-8 text-center text-xl">Previous identity</h2>
             <div className="m-auto max-w-[1014px]">
-                <Image src="/ioverlay/previous.png" width={1014} height={924} alt="" />
+                <Image src="/project/ioverlay/previous.png" width={1014} height={924} alt="" />
             </div>
         </div>
     );
