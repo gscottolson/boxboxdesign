530c19ba703aee220ca043213d23118febcbb681
Scott Olson <s.olson@microsoft.com>
2024-02-07 11:29:27 -0800

    Update copy, add Windows icon screenshot

--- app/(ioverlay)/ioverlay/page.tsx
+++ app/(ioverlay)/ioverlay/page.tsx
@@ -29,6 +29,12 @@ export default function Page() {
                 height={700}
                 alt="iOverlay identity on a light background"
             />
+            <Image
+                src="/project/ioverlay/windows-icon.jpg"
+                width={1280}
+                height={640}
+                alt="iOverlay search results in Windows OS"
+            />
 
             <h2 className="mb-4 mt-8 text-center text-xl">3d explorations</h2>
 
@@ -43,23 +49,63 @@ export default function Page() {
             </div>
 
             <h2 className="mb-4 mt-8 text-center text-xl">Concept iteration, round 1</h2>
+
+            <p className="mx-auto mb-16 max-w-[880px] px-[40px] text-xl">
+                This first collection of ideas was presented as feedback to Joeri on the iOverlay Discord. These were a
+                few options to improve legibility of the icon when viewed at small sizes on Windows.
+            </p>
+
             <div className="m-auto max-w-[1050px]">
-                <Image src="/project/ioverlay/round001.png" width={1050} height={524} alt="" />
+                <Image
+                    src="/project/ioverlay/round001.png"
+                    width={1050}
+                    height={524}
+                    alt="Visual explorations of lowercase i and capital O and the workmark iOverlay."
+                />
             </div>
 
             <h2 className="mb-4 mt-8 text-center text-xl">Concept iteration, round 2</h2>
+            <p className="mx-auto mb-16 max-w-[880px] px-[40px] text-xl">
+                A second round of ideas based on input from Joeri. Keeping the forms of a lowercase i and uppercase O.
+                Exploring ideas like automobile guages, tunnels, eyeballs and translucent layers. Some of these are
+                great, many are rough.
+            </p>
+
             <div className="m-auto max-w-[1050px]">
-                <Image src="/project/ioverlay/round002.png" width={1050} height={1284} alt="" />
+                <Image
+                    src="/project/ioverlay/round002.png"
+                    width={1050}
+                    height={1284}
+                    alt="Identity explorations for iOverlay using various black, white and green shapes."
+                />
             </div>
 
             <h2 className="mb-4 mt-8 text-center text-xl">Concept iteration, round 3</h2>
+            <p className="mx-auto mb-16 max-w-[880px] px-[40px] text-xl">
+                The third round was about refinement and focus. Color steps, green dots, and wordmarks are starting to
+                take final form.
+            </p>
             <div className="m-auto max-w-[1050px]">
-                <Image src="/project/ioverlay/round003.png" width={1050} height={1020} alt="" />
+                <Image
+                    src="/project/ioverlay/round003.png"
+                    width={1050}
+                    height={1020}
+                    alt="Focused and refined iOverlay logos with monochromatic and color treatments."
+                />
             </div>
 
             <h2 className="mb-4 mt-8 text-center text-xl">Previous identity</h2>
+            <p className="mx-auto mb-16 max-w-[880px] px-[40px] text-xl">
+                The original art, featuring the classic typeface Snap ITC Std. Retaining the simplicity of black and
+                white while keeping the green accents were key considerations during the design process.
+            </p>
             <div className="m-auto max-w-[1014px]">
-                <Image src="/project/ioverlay/previous.png" width={1014} height={924} alt="" />
+                <Image
+                    src="/project/ioverlay/previous.png"
+                    width={1014}
+                    height={924}
+                    alt="Visuals from the previous identity for iOverlay."
+                />
             </div>
         </div>
     );
