69186e5f88617913add1f2052beef5e255673c00
gscottolson <scott@gscottolson.com>
2024-03-11 18:13:47 -0700

    Umami analytics

--- app/(ioverlay)/layout.tsx
+++ app/(ioverlay)/layout.tsx
@@ -6,9 +6,16 @@ import { outfit } from '../fonts';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" className="bg-[#EEEEEE]">
+            <head>
+                <script
+                    defer
+                    src="https://analytics.us.umami.is/script.js"
+                    data-website-id="ea42990e-0591-454e-b494-416a0703ceef"
+                ></script>
+            </head>
             <body className={outfit.className}>
                 {children}
-                <Analytics />
+                {/* <Analytics /> */}
                 <SpeedInsights />
             </body>
         </html>
