7c0f58b8967ae06eab8513c513402dcc77744ffc
Scott Olson <s.olson@microsoft.com>
2024-04-25 09:26:23 -0700

    Generated sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+    siteUrl: "https://boxbox.design",
+    generateIndexSitemap: false,
+    exclude: [
+        "/public/pdf",
+        "/logo-test",
+        "/iracing-graphics",
+        "/ioverlay",
+    ],
+  }
