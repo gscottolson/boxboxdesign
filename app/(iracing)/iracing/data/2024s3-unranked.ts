aca9a59deda93c13fb8f9e25ae016d6593ab1e3d
gscottolson <scott@gscottolson.com>
2024-05-30 05:40:11 -0700

    Data files copied over from 2024s2

---
+++ app/(iracing)/iracing/data/2024s3-unranked.ts
@@ -0,0 +1,40 @@
+import { OfficialSeries } from '../types';
+
+export const iRacing2024S3UnrankedSeries: OfficialSeries[] = [
+    {
+        name: 'Carburetor Cup',
+        seriesId: 'f279-CarburetorCupFixed',
+        season: '2024s3',
+        discipline: 'Unranked',
+        licenseClass: 'Rookie',
+        srcLight: '/thumbnails/iracing/2024s3/f279-CarburetorCupFixed-Light.png',
+        srcDark: '/thumbnails/iracing/2024s3/f279-CarburetorCupFixed-Dark.png',
+        pdfLight: '/pdf/iracing/2024s3-unranked/f279-CarburetorCupFixed-Light.pdf',
+        pdfDark: '/pdf/iracing/2024s3-unranked/f279-CarburetorCupFixed-Dark.pdf',
+        setup: 'fixed',
+    },
+    {
+        name: 'Dallara Dash',
+        seriesId: 'e1f8-DallaraDashFixed',
+        season: '2024s3',
+        discipline: 'Unranked',
+        licenseClass: 'Rookie',
+        srcLight: '/thumbnails/iracing/2024s3/e1f8-DallaraDashFixed-Light.png',
+        srcDark: '/thumbnails/iracing/2024s3/e1f8-DallaraDashFixed-Dark.png',
+        pdfLight: '/pdf/iracing/2024s3-unranked/e1f8-DallaraDashFixed-Light.pdf',
+        pdfDark: '/pdf/iracing/2024s3-unranked/e1f8-DallaraDashFixed-Dark.pdf',
+        setup: 'fixed',
+    },
+    {
+        name: 'PickUp Cup',
+        seriesId: 'a0fc-PickUpCupFixed',
+        season: '2024s3',
+        discipline: 'Unranked',
+        licenseClass: 'Rookie',
+        srcLight: '/thumbnails/iracing/2024s3/a0fc-PickUpCupFixed-Light.png',
+        srcDark: '/thumbnails/iracing/2024s3/a0fc-PickUpCupFixed-Dark.png',
+        pdfLight: '/pdf/iracing/2024s3-unranked/a0fc-PickUpCupFixed-Light.pdf',
+        pdfDark: '/pdf/iracing/2024s3-unranked/a0fc-PickUpCupFixed-Dark.pdf',
+        setup: 'fixed',
+    },
+];
