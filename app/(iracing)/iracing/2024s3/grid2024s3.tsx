fd5ba89d52a398f02f8f41a8dc661928d3c343a3
gscottolson <scott@gscottolson.com>
2024-05-30 12:41:12 -0700

    Grid can render children within

--- app/(iracing)/iracing/2024s3/grid2024s3.tsx
+++ app/(iracing)/iracing/2024s3/grid2024s3.tsx
@@ -1,11 +1,25 @@
 import { SeriesCard } from './series-card';
 import { OfficialSeries } from '../types';
+import { Fragment } from 'react';
 
-export function Grid2024S3({ seriesArray }: { seriesArray: OfficialSeries[] }) {
+const DEFAULT_INDEX = 3;
+
+export function Grid2024S3({
+    seriesArray,
+    childGridIndex = DEFAULT_INDEX,
+    children,
+}: {
+    seriesArray: OfficialSeries[];
+    childGridIndex?: number | null;
+    children?: React.ReactNode;
+}) {
     return !seriesArray ? null : (
         <div className="m-auto flex min-h-[480px] max-w-7xl flex-wrap justify-center gap-6 px-2 pb-12 text-base md:px-8">
             {seriesArray.map((series, index) => (
-                <SeriesCard key={series.name} series={series} priority={index < 8} />
+                <Fragment key={series.name}>
+                    <SeriesCard series={series} priority={index < 8} />
+                    {childGridIndex && index === childGridIndex && <Fragment key="inject">{children}</Fragment>}
+                </Fragment>
             ))}
         </div>
     );
