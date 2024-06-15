bac930d00e909971fcb2b48753a18aae5b38238e
gscottolson <scott@gscottolson.com>
2024-06-15 14:16:42 -0700

    Fix donate-within-grid for mobile sizes

--- app/(iracing)/iracing/2024s3/donate-within-grid.tsx
+++ app/(iracing)/iracing/2024s3/donate-within-grid.tsx
@@ -2,11 +2,13 @@ import { BuyMeACoffee } from './icons';
 
 export function DonateWithinGrid() {
     return (
-        <div className="group mx-24 w-2/3 rounded-2xl bg-white300 px-8 py-16 text-center text-base font-light text-teal800 dark:bg-gray-950 dark:text-gray-400">
-            <div className="m-auto text-xl">
-                <p className="m-auto mb-8 w-[70%] max-w-[560px]">
-                    Hello. <span className="font-medium"> I’m Scott</span>, a one-man pit crew at BoxBoxDesign.
-                    <span className="font-medium"> Please donate</span> to help keep these schedules free.
+        <div className="group mx-24 rounded-2xl bg-white300 px-8 py-16 md:w-2/3 dark:bg-gray-950">
+            <div className="m-auto text-center text-xl font-light text-teal800 dark:text-gray-400">
+                <p className="m-auto mb-8 min-w-[300px] max-w-[560px] md:w-[70%]">
+                    Hello. <span className="font-medium dark:text-gray-200"> I’m Scott</span>, a one-man pit crew at
+                    BoxBoxDesign.
+                    <span className="font-medium dark:text-gray-200"> Please donate</span> to help keep these schedules
+                    free.
                 </p>
                 <div className="m-auto h-16 w-56 text-[#32545B] hover:text-gray-950 dark:text-gray-300 hover:dark:text-gray-100">
                     <a
