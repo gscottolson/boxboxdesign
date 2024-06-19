321eeb56c01b9151fa5a94b320b04d7e8ed9322c
Scott Olson <s.olson@microsoft.com>
2024-06-19 07:30:21 -0700

    Add season note

--- app/(iracing)/iracing/2024s2/layout.tsx
+++ app/(iracing)/iracing/2024s2/layout.tsx
@@ -2,6 +2,8 @@ import { getSiteTitle, getSiteTitleTemplate } from '@/app/site';
 import { Metadata } from 'next';
 import Balancer from 'react-wrap-balancer';
 import ModeToggle from './mode-toggle';
+import { getDisciplineURL } from '../data/series-util';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
     title: {
@@ -16,6 +18,15 @@ export default function Layout(props: { children: React.ReactNode; modal: React.
             <div className="relative flex-grow bg-[#BDCDD2] pt-16 dark:bg-[#282C2D]">
                 <ModeToggle />
                 <header className="text-center tracking-tighter text-gray700 dark:text-gray-200">
+                    <div className="bg-slate-100 px-24 py-2 leading-[1.2] tracking-wider dark:bg-slate-950">
+                        <Balancer>
+                            You are viewing schedules for 2024 season 2, a past season of iRacing. The current set is{' '}
+                            <Link className="font-medium text-slate-400" href={getDisciplineURL('Formula', '2024s3')}>
+                                2024 season 3
+                            </Link>
+                            .
+                        </Balancer>
+                    </div>
                     <h1 className=" px-10 pt-12 text-4xl font-light leading-none antialiased">
                         <Balancer>{getSiteTitle()}</Balancer>
                     </h1>
