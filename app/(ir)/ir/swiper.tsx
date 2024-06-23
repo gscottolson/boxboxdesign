99a32555202aaadff3fd4672394b4abaf0732306
Scott Olson <s.olson@microsoft.com>
2024-06-23 12:28:05 -0700

    POC update

--- app/(ir)/ir/swiper.tsx
+++ app/(ir)/ir/swiper.tsx
@@ -4,11 +4,31 @@ import { useRef, useEffect, useState } from 'react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { SwiperContainer, register } from 'swiper/element/bundle';
+import { Swiper } from 'swiper/types';
 register();
 
 const MyComponent = () => {
     const swiperElRef = useRef<SwiperContainer>(null);
     const [isClient, setIsClient] = useState(false);
+
+    const posterIDs = [
+        '4d87-GlobalMazdaFixed',
+        '0422-FerrariGT3Fixed',
+        'a3f6-GRCupFixed',
+        'e7fa-PCCOpen',
+        '9942-GT4Fixed',
+        'ed6d-GlobalChallengeFixed',
+        '38a2-ClioFixed',
+        'a158-TCRFixed',
+        '79aa-SpecRacerOpen',
+        'eb70-MustangFixed',
+        '70e4-MissionRFixed',
+        'e764-TCRChallengeOpen',
+        '455b-RingMeisterFixed',
+        'bff8-PorscheCupOpen',
+        '4f27-PorscheCupFixed',
+        'b35b-GTEnduranceOpen',
+    ];
 
     useEffect(() => {
         setIsClient(true);
@@ -27,11 +47,11 @@ const MyComponent = () => {
     }, []);
 
     return !isClient ? null : (
-        <div className="flex h-dvh w-full items-center">
+        <div className="flex h-dvh w-full">
             <swiper-container
-                class="m-auto w-2/3"
+                class="swiper swiper-3d m-auto w-11/12"
                 speed="500"
-                css-mode="true"
+                css-mode="false"
                 navigation="true"
                 pagination="true"
                 loop="true"
@@ -39,16 +59,17 @@ const MyComponent = () => {
                 centered-slides="true"
                 slides-per-view="3"
                 effect="coverflow"
-                coverflow-effect-rotate="60"
-                coverflow-effect-stretch="0"
-                coverflow-effect-depth="200"
+                coverflow-effect-rotate="30"
+                coveflow-effect-modifier="1"
+                coverflow-effect-depth="100"
                 coverflow-effect-scale="0.9"
-                grabCursor="true"
+                coverflow-effect-slide-shadows="true"
+                coverflow-effect-stretch="0"
             >
-                {[1, 2, 3, 4, 5, 6].map((value) => (
-                    <swiper-slide class="flex h-96 items-center justify-center" key={value}>
-                        <div className="swiper-slide-transform flex aspect-[3/4] h-full items-center justify-center bg-slate-300">
-                            Slide {value}
+                {posterIDs.map((value) => (
+                    <swiper-slide class="flex h-auto items-center justify-center" key={value}>
+                        <div className="swiper-slide-transform aspect-[3/4] bg-slate-300">
+                            <img className="h-auto w-full" src={`/iracing/posters/2024s3/${value}-Dark.png`} />
                         </div>
                     </swiper-slide>
                 ))}
