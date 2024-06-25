00ab0e89d664cef7456b52d4a0d110d25d8091c9
gscottolson <scott@gscottolson.com>
2024-06-25 06:33:57 -0700

    Far better Swiper POC

--- app/(ir)/ir/swiper.tsx
+++ app/(ir)/ir/swiper.tsx
@@ -1,77 +1,102 @@
 'use client';
 
 import { useRef, useEffect, useState } from 'react';
-import 'swiper/css';
-import 'swiper/css/pagination';
+import 'swiper/css/bundle';
 import { SwiperContainer, register } from 'swiper/element/bundle';
-import { Swiper } from 'swiper/types';
 register();
+
+// swiper parameters
+const swiperParams = {
+    // pagination: true,
+    // pagination-dynamic-bullets="true"
+    cssMode: false,
+    grabCursor: true,
+    speed: 100,
+    centeredSlides: true,
+    // slidesPerView: 1.2,
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: '20',
+        modifier: '1',
+        depth: '10',
+        scale: '0.95',
+        slideShadows: 'true',
+        stretch: '0',
+    },
+    breakpoints: {
+        320: { slidesPerView: 1.2 },
+        640: { slidesPerView: 2 },
+        1024: { slidesPerView: 3 },
+    },
+    on: {
+        init() {
+            // ...
+        },
+    },
+};
+
+const posterIDs = [
+    '2cd1-StockCarBrasilFixed',
+    '4a4b-BMWPowerTourFixed',
+    '4d87-GlobalMazdaFixed',
+    '4f27-PorscheCupFixed',
+    '9b1b-IMSAEnduranceOpen',
+    '9d22-GTEOpen',
+    '9df6-LMP3Fixed',
+    '38a2-ClioFixed',
+    '70e4-MissionRFixed',
+    '79aa-SpecRacerOpen',
+    '149a-IMSAVintageOpen',
+    '0422-FerrariGT3Fixed',
+    '455b-RingMeisterFixed',
+    '458d-IMSAFixed',
+    '8552-AdvancedMazdaOpen',
+    '8903-SupercarsAusOpen',
+    '9942-GT4Fixed',
+    'a3f6-GRCupFixed',
+    'a158-TCRFixed',
+    'aa1b-LMP2Fixed',
+    'b1e2-IMSAOpen',
+    'b35b-GTEnduranceOpen',
+    'bb65-SupercarsOpen',
+    'bff8-PorscheCupOpen',
+    'c5d8-GT3Fixed',
+    'ce87-IMSAPilotOpen',
+    'd034-SCCOpen',
+    'd829-RainMasterFixed',
+    'e7fa-PCCOpen',
+    'e764-TCRChallengeOpen',
+    'eb70-MustangFixed',
+    'ec92-RadicalCupFixed',
+    'ed6d-GlobalChallengeFixed',
+];
 
 const MyComponent = () => {
     const swiperElRef = useRef<SwiperContainer>(null);
     const [isClient, setIsClient] = useState(false);
 
-    const posterIDs = [
-        '4d87-GlobalMazdaFixed',
-        '0422-FerrariGT3Fixed',
-        'a3f6-GRCupFixed',
-        'e7fa-PCCOpen',
-        '9942-GT4Fixed',
-        'ed6d-GlobalChallengeFixed',
-        '38a2-ClioFixed',
-        'a158-TCRFixed',
-        '79aa-SpecRacerOpen',
-        'eb70-MustangFixed',
-        '70e4-MissionRFixed',
-        'e764-TCRChallengeOpen',
-        '455b-RingMeisterFixed',
-        'bff8-PorscheCupOpen',
-        '4f27-PorscheCupFixed',
-        'b35b-GTEnduranceOpen',
-    ];
+    useEffect(() => setIsClient(true), []);
 
     useEffect(() => {
-        setIsClient(true);
-
         if (swiperElRef && swiperElRef.current) {
-            // listen for Swiper events using addEventListener
-            swiperElRef.current.addEventListener('swiperprogress', (e) => {
-                // const [swiper, progress] = e.detail;
-                console.log(e.target);
-            });
-
-            swiperElRef.current.addEventListener('swiperslidechange', (e) => {
-                console.log('slide changed');
-            });
+            Object.assign(swiperElRef.current, swiperParams);
+            swiperElRef.current.initialize();
+        } else {
+            console.error('Expected swiperElRef to reference swiper element');
         }
-    }, []);
+    }, [isClient]);
 
     return !isClient ? null : (
         <div className="flex h-dvh w-full">
-            <swiper-container
-                class="swiper swiper-3d m-auto w-11/12"
-                speed="500"
-                css-mode="false"
-                navigation="true"
-                pagination="true"
-                loop="true"
-                grab-cursor="true"
-                centered-slides="true"
-                slides-per-view="3"
-                effect="coverflow"
-                coverflow-effect-rotate="30"
-                coveflow-effect-modifier="1"
-                coverflow-effect-depth="100"
-                coverflow-effect-scale="0.9"
-                coverflow-effect-slide-shadows="true"
-                coverflow-effect-stretch="0"
-            >
-                {posterIDs.map((value) => (
-                    <swiper-slide class="flex h-auto items-center justify-center" key={value}>
-                        <div className="swiper-slide-transform aspect-[3/4] bg-slate-300">
-                            <img className="h-auto w-full" src={`/iracing/posters/2024s3/${value}-Dark.png`} />
-                        </div>
-                    </swiper-slide>
+            <swiper-container ref={swiperElRef} init="false" class="swiper m-auto w-11/12 py-48">
+                {posterIDs.map((value, index) => (
+                    <div
+                        slot={`slide-${index}`}
+                        className="slide-content flex h-auto items-center justify-center"
+                        key={value}
+                    >
+                        <img className="h-auto w-full" src={`/iracing/posters/2024s3/${value}-Dark.png`} />
+                    </div>
                 ))}
             </swiper-container>
         </div>
