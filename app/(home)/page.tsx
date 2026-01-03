79374a2fa539e0fc41c27fae4d59a540fd03cc76
gscottolson <scott@gscottolson.com>
2026-01-03 10:17:44 -0800

    New home layout, updates to iOverlay

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,33 +5,93 @@ import { Metadata } from 'next';
 import './page.css';
 
 import { getSiteTitle as getiRacingSiteTitle } from '../site';
-import { getDisciplineURL } from '../(iracing)/iracing/data/series-util';
+import { getDisciplineURL as getPostersURL } from '../(iracing)/iracing/data/series-util';
+import LogoIcon from './logo-icon';
+import LogoText from './logo-text';
 
 export const metadata: Metadata = {
     title: 'BoxBoxDesign, makers of fine digital goods',
 };
 
 export default function Page() {
-    return (
-        <main className="flex min-h-dvh flex-col items-center justify-center gap-16 p-6 text-[#304F70]">
-            <Image width={420} height={420} alt="BoxBoxDesign logo" src="/home/boxbox-circle.svg" />
+    const formatDate = () => {
+        const date = new Date();
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = date.toLocaleString('en-US', { month: 'short' }).toUpperCase();
+        const year = date.getFullYear();
+        return `${day} ${month} ${year}`;
+    };
 
-            <div className="flex flex-col items-center justify-center text-xl leading-[24px]">
-                <p className="pb-2 font-light">Current projects:</p>
-                <ul className="pb-16 text-center font-medium">
-                    <li>
-                        <Link href={getDisciplineURL('Formula', '2024s3')} className="hover:underline">
-                            {getiRacingSiteTitle()}
-                        </Link>
-                    </li>
-                </ul>
-                <p className="pb-2 font-light">
-                    Get in touch:
-                    <span id="email" className="pl-2 font-medium">
-                        scott<b>nobots</b>@boxbox<b>.nobots</b>.design
-                    </span>
-                </p>
-            </div>
-        </main>
+    return (
+        <div className="overflow-x-hidden">
+            <main className="flex min-h-dvh flex-col items-center justify-center text-[#304F70] lg:flex-row lg:gap-16">
+                <header className="relative flex w-[282px] flex-col items-center justify-center gap-0 pr-[14px] font-thin uppercase">
+                    <div className="init-sequence text-right"></div>
+
+                    <div className="flex flex-col items-end">
+                        <LogoIcon />
+
+                        <div className="flex h-12 items-center justify-end">
+                            <LogoText className="w-[230px]" />
+                        </div>
+                        <p className="text-right font-medium">Makers of fine digital goods</p>
+                        <p>{formatDate()}</p>
+                    </div>
+                    <div className="footer-sequence relative w-full text-right"></div>
+                </header>
+
+                <nav className="text-l flex w-auto flex-col justify-start pb-6">
+                    <p className="pb-2 pl-8 pt-0 font-semibold uppercase tracking-[0.15em] lg:pt-48">Projects</p>
+                    <ul className="flex flex-col justify-center gap-4 px-8 font-medium sm:flex-row">
+                        <li className="w-auto">
+                            <Link
+                                href={getPostersURL('Formula', '2024s3')}
+                                className="inline-block"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <div className="image-container relative inline-block opacity-75 mix-blend-multiply transition-opacity duration-500 ease-in-out hover:opacity-85">
+                                    <Image
+                                        src="/home/posters-hover.png"
+                                        width="280"
+                                        height="136"
+                                        alt="{getiRacingSiteTitle()}"
+                                        className="gb-image relative h-auto opacity-50 brightness-[300%] contrast-[120%] saturate-0"
+                                    />
+                                    <div className="image-overlay absolute left-0 top-0 h-full w-full bg-[#1a575a] opacity-100 mix-blend-soft-light" />
+                                </div>
+                                <p className="project-link-text pt-2 text-left font-light uppercase leading-[1.1] hover:border-b hover:border-b-[0.5px] hover:border-[#9bb0a4]">
+                                    {getiRacingSiteTitle()}
+                                </p>
+                            </Link>
+                        </li>
+                        <li className="w-auto">
+                            <Link href="/ioverlay" className="inline-block" target="_blank" rel="noopener noreferrer">
+                                <div className="image-container relative inline-block opacity-75 mix-blend-multiply transition-opacity duration-500 ease-in-out hover:opacity-85">
+                                    <Image
+                                        width="280"
+                                        height="136"
+                                        alt="iOverlay composition"
+                                        src="/home/ioverlay-hover.png"
+                                        className="ioverlay-image relative h-auto opacity-50 brightness-[120%] contrast-[90%] saturate-0"
+                                    />
+                                    <div className="image-overlay absolute left-0 top-0 h-full w-full bg-[#1a575a] opacity-100 mix-blend-soft-light" />
+                                </div>
+                                <p className="project-link-text pt-2 text-left font-light uppercase leading-[1.1] hover:border-b-[0.5px] hover:border-[#9bb0a4]">
+                                    iOverlay
+                                </p>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </main>
+            <footer className="w-full overflow-x-hidden px-4 pb-8 pt-8 text-center font-light uppercase tracking-[0.15em]">
+                <span className="whitespace-nowrap font-medium text-[#9bb0a4]">Get in touch</span>
+                <span id="email" className="whitespace-nowrap pl-4">
+                    scott<b>nobots</b>
+                    <span className="text-xl font-medium text-[#9bb0a4]">@</span>boxbox<b>.nobots</b>.design
+                </span>
+            </footer>
+        </div>
     );
 }
--- app/(ioverlay)/ioverlay/page.tsx
+++ app/(ioverlay)/ioverlay/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import { Metadata } from 'next';
+import Link from 'next/link';
+import LogoText from '../../(home)/logo-text';
 
 export const metadata: Metadata = {
     title: 'iOverlay Identity Project',
@@ -7,106 +9,137 @@ export const metadata: Metadata = {
 
 export default function Page() {
     return (
-        <div className="m-auto max-w-[1280px] py-16 text-[#484848]">
-            <h1 className="mb-8 text-center text-3xl">iOverlay Identity Project</h1>
+        <div>
+            <div className="m-auto max-w-[1280px] py-16 text-xl text-[#484848]">
+                <ContentWrap>
+                    <h1 className="mb-1 text-3xl leading-snug">iOverlay Identity Project</h1>
+                    <h2 className="mb-8 text-2xl uppercase">Fall 2023</h2>
+                    <p className="mb-16">
+                        BoxBoxDesign partnered with Joeri to develop a new visual identity for iOverlay, a real-time
+                        informational overlay app for the iRacing simulator. iOverlay displays race standings,
+                        positional data, track maps, fuel calculations, and other live race metrics.
+                    </p>
+                </ContentWrap>
 
-            <p className="mx-auto mb-16 max-w-[880px] px-[40px] text-xl">
-                BoxBoxDesign had the opportunity to work with Joeri at iOverlay on a new identity for his app. iOverlay
-                is an informational overlay that works with the popular online racing simulator iRacing. iOverlay
-                provides race information in real-time and allows you to see race standings, your race position relative
-                to other drivers, track maps, fuel calculators and more.
-            </p>
-
-            <Image
-                src="/project/ioverlay/ioverlay-dark.png"
-                width={1280}
-                height={700}
-                alt="iOverlay identity on a dark background"
-            />
-            <Image
-                src="/project/ioverlay/ioverlay-light.png"
-                width={1280}
-                height={700}
-                alt="iOverlay identity on a light background"
-            />
-            <Image
-                src="/project/ioverlay/windows-icon.jpg"
-                width={1280}
-                height={640}
-                alt="iOverlay search results in Windows OS"
-            />
-
-            <h2 className="mb-4 mt-8 text-center text-xl">3d explorations</h2>
-
-            <div className="grid grid-flow-row grid-cols-2 gap-[12px]">
-                <Image src="/project/ioverlay/closeup.jpg" width={634} height={634} alt="" />
-                <Image src="/project/ioverlay/portrait.jpg" width={634} height={634} alt="" />
-                <Image className="col-span-2" src="/project/ioverlay/wordmark.jpg" width={1280} height={634} alt="" />
-                <Image src="/project/ioverlay/clay001.jpg" width={634} height={634} alt="" />
-                <Image src="/project/ioverlay/clay002.jpg" width={634} height={634} alt="" />
-                <Image src="/project/ioverlay/isometric.jpg" width={634} height={634} alt="" />
-                <Image src="/project/ioverlay/steps.jpg" width={634} height={634} alt="" />
-            </div>
-
-            <h2 className="mb-4 mt-8 text-center text-xl">Concept iteration, round 1</h2>
-
-            <p className="mx-auto mb-16 max-w-[880px] px-[40px] text-xl">
-                This first collection of ideas was presented as feedback to Joeri on the iOverlay Discord. These were a
-                few options to improve legibility of the icon when viewed at small sizes on Windows.
-            </p>
-
-            <div className="m-auto max-w-[1050px]">
                 <Image
-                    src="/project/ioverlay/round001.png"
-                    width={1050}
-                    height={524}
-                    alt="Visual explorations of lowercase i and capital O and the workmark iOverlay."
+                    src="/project/ioverlay/ioverlay-dark.png"
+                    width={1280}
+                    height={700}
+                    alt="iOverlay identity on a dark background"
                 />
-            </div>
-
-            <h2 className="mb-4 mt-8 text-center text-xl">Concept iteration, round 2</h2>
-            <p className="mx-auto mb-16 max-w-[880px] px-[40px] text-xl">
-                A second round of ideas based on input from Joeri. Keeping the forms of a lowercase i and uppercase O.
-                Exploring ideas like automobile guages, tunnels, eyeballs and translucent layers. Some of these are
-                great, many are rough.
-            </p>
-
-            <div className="m-auto max-w-[1050px]">
                 <Image
-                    src="/project/ioverlay/round002.png"
-                    width={1050}
-                    height={1284}
-                    alt="Identity explorations for iOverlay using various black, white and green shapes."
+                    src="/project/ioverlay/ioverlay-light.png"
+                    width={1280}
+                    height={700}
+                    alt="iOverlay identity on a light background"
                 />
-            </div>
-
-            <h2 className="mb-4 mt-8 text-center text-xl">Concept iteration, round 3</h2>
-            <p className="mx-auto mb-16 max-w-[880px] px-[40px] text-xl">
-                The third round was about refinement and focus. Color steps, green dots, and wordmarks are starting to
-                take final form.
-            </p>
-            <div className="m-auto max-w-[1050px]">
                 <Image
-                    src="/project/ioverlay/round003.png"
-                    width={1050}
-                    height={1020}
-                    alt="Focused and refined iOverlay logos with monochromatic and color treatments."
+                    src="/project/ioverlay/windows-icon.jpg"
+                    width={1280}
+                    height={640}
+                    alt="iOverlay search results in Windows OS"
                 />
-            </div>
 
-            <h2 className="mb-4 mt-8 text-center text-xl">Previous identity</h2>
-            <p className="mx-auto mb-16 max-w-[880px] px-[40px] text-xl">
-                The original art, featuring the classic typeface Snap ITC Std. Retaining the simplicity of black and
-                white while keeping the green accents were key considerations during the design process.
-            </p>
-            <div className="m-auto max-w-[1014px]">
-                <Image
-                    src="/project/ioverlay/previous.png"
-                    width={1014}
-                    height={924}
-                    alt="Visuals from the previous identity for iOverlay."
-                />
+                <ContentWrap>
+                    <h2 className="mb-4 mt-32 text-2xl font-medium uppercase">3d explorations</h2>
+                    <p className="mb-16">Refining an understanding of how form influences emotional tone.</p>
+                </ContentWrap>
+                <div className="grid grid-flow-row grid-cols-2 gap-[12px]">
+                    <Image src="/project/ioverlay/closeup.jpg" width={634} height={634} alt="" />
+                    <Image src="/project/ioverlay/portrait.jpg" width={634} height={634} alt="" />
+                    <Image
+                        className="col-span-2"
+                        src="/project/ioverlay/wordmark.jpg"
+                        width={1280}
+                        height={634}
+                        alt=""
+                    />
+                    <Image src="/project/ioverlay/clay001.jpg" width={634} height={634} alt="" />
+                    <Image src="/project/ioverlay/clay002.jpg" width={634} height={634} alt="" />
+                    <Image src="/project/ioverlay/isometric.jpg" width={634} height={634} alt="" />
+                    <Image src="/project/ioverlay/steps.jpg" width={634} height={634} alt="" />
+                </div>
+
+                <ContentWrap>
+                    <h2 className="mb-4 mt-32 text-2xl font-medium uppercase">Round 1: Initial concepts</h2>
+
+                    <p className="mb-16">
+                        Early logo explorations focused on improving icon legibility at small sizes on Windows.
+                    </p>
+                </ContentWrap>
+
+                <div className="m-auto max-w-[1050px]">
+                    <Image
+                        src="/project/ioverlay/round001.png"
+                        width={1050}
+                        height={524}
+                        alt="Visual explorations of lowercase i and capital O and the workmark iOverlay."
+                    />
+                </div>
+
+                <ContentWrap>
+                    <h2 className="mb-4 mt-32 text-2xl font-medium uppercase">Round 2: Refined ideas</h2>
+                    <p className="mx-auto mb-16">
+                        Based on feedback from the iOverlay Discord community, we refined the lowercase “i” and
+                        uppercase “O,” and explored visual motifs inspired by automotive gauges, tunnels, eyes, and
+                        layered transparencies.
+                    </p>
+                </ContentWrap>
+
+                <div className="m-auto max-w-[1050px]">
+                    <Image
+                        src="/project/ioverlay/round002.png"
+                        width={1050}
+                        height={1284}
+                        alt="Identity explorations for iOverlay using various black, white and green shapes."
+                    />
+                </div>
+
+                <ContentWrap>
+                    <h2 className="mb-4 mt-32 text-2xl font-medium uppercase">Round 3: Final development</h2>
+                    <p className="mx-auto mb-16">
+                        In the final stage, we sharpened the forms, introduced color steps and accent dots, and
+                        finalized wordmark treatments.
+                    </p>
+                </ContentWrap>
+
+                <div className="m-auto max-w-[1050px]">
+                    <Image
+                        src="/project/ioverlay/round003.png"
+                        width={1050}
+                        height={1020}
+                        alt="Focused and refined iOverlay logos with monochromatic and color treatments."
+                    />
+                </div>
+
+                <ContentWrap>
+                    <h2 className="mb-4 mt-32 text-2xl font-medium uppercase">Previous identity</h2>
+                    <p className="mx-auto mb-16">
+                        The original logo used a simple black-and-white palette with green accents and the Snap ITC Std
+                        typeface. Our redesign builds on that simplicity while enhancing clarity and visual cohesion.
+                    </p>
+                </ContentWrap>
+                <div className="m-auto max-w-[1014px]">
+                    <Image
+                        src="/project/ioverlay/previous.png"
+                        width={1014}
+                        height={924}
+                        alt="Visuals from the previous identity for iOverlay."
+                    />
+                </div>
             </div>
+            <footer className="mt-24 bg-[#E4EDE6] py-20 text-center font-light uppercase tracking-[0.15em]">
+                <Link
+                    href="/"
+                    className="mt-8 flex items-center justify-center gap-2 text-xl text-slate-500 opacity-50 transition-opacity duration-150 hover:opacity-100"
+                >
+                    ← Back to <LogoText className="h-5 w-44 " />
+                </Link>
+            </footer>
         </div>
     );
 }
+
+const ContentWrap = ({ children }: { children: React.ReactNode }) => {
+    return <div className="mx-auto mb-8 max-w-[880px] px-[40px]">{children}</div>;
+};
